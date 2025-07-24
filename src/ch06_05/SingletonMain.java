27862d14419ba7d7bddc4e75f21dbcad1d1c3abf
khyuna0 <khohyuna0102@gmail.com>
2025-07-24 12:00:14 +0900

    싱글톤 예제 추가

--- src/ch06_05/SingletonMain.java
+++ src/ch06_05/SingletonMain.java
@@ -4,7 +4,17 @@ public class SingletonMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		SingletonTest single1 = SingletonTest.getInstance(); // return 값으로 SingletonTest 객체를 반환
 
+		SingletonTest single2 = SingletonTest.getInstance();
+		
+		if (single1 == single2) {
+			System.out.println("동일");
+		}	
+		
+		
+		
 	}
 
 }
--- src/ch06_05/SingletonTest.java
+++ src/ch06_05/SingletonTest.java
@@ -3,13 +3,13 @@ package ch06_05;
 public class SingletonTest { // 메모리에 반드시 하나만 존재해야 하는 인스턴스 ( 없거나 하나거나...)
 	
 	//  private static 클래스이름 변수이름 형식
-	private static SingletonTest singletonTest = new SingletonTest(); // 이 클래스 내에서만 사용 가능한 변수
+	private static SingletonTest singletonTest = new SingletonTest(); // 이 클래스 내에서만 사용 가능한 변수 -> 정적 필드와 비슷
 
-	private SingletonTest() { // private 생성자로 호출 막음. 외부( 이 클래스 외부)에서 호출 불가
+	private SingletonTest() { // private 생성자로 호출 막음. 외부(이 클래스 외부)에서 호출 불가
 	
 	}
 	
-	// 인스턴스 생성 메소드
+	// 인스턴스 생성 메소드 | 메소드는 외부에서 호출 가능. 자체적 객체 반환
 	public static SingletonTest getInstance() { // 메소드 이름 : getInstance() 관례
 		return singletonTest;
 	}
